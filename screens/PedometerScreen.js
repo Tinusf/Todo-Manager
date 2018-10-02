71c9d1734d36f0952c950981f9adc823daac6caa
Tinus Flagstad <tinus@flagstad.as>
2018-10-02 15:34:54 +0200

    Change indent from 4 spaces to 2

--- screens/PedometerScreen.js
+++ screens/PedometerScreen.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import {
-    Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    Alert,
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
 } from 'react-native';
 
 export default class PedometerScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Pedometer view',
-    };
+  static navigationOptions = {
+    title: 'Pedometer view',
+  };
 
-    render() {
-        return (
-            <Text>Pedometer screen</Text>
-        );
-    }
+  render() {
+    return (
+      <Text>Pedometer screen</Text>
+    );
+  }
 }
