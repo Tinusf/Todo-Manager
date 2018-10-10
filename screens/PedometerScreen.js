395327b9d8564f602cbfac809d89d78025715cfa
martinbg1 <martin.b.gjerde@gmail.com>
2018-10-10 11:15:43 +0200

    Add pedometer functionality to PedometerScreen and add PedometerScreen-test.js #10

--- screens/PedometerScreen.js
+++ screens/PedometerScreen.js
@@ -1,23 +1,91 @@
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Alert,
-} from 'react-native';
+import Expo from "expo";
+import React from "react";
+import { Pedometer } from "expo";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class PedometerScreen extends React.Component {
   static navigationOptions = {
-    title: 'Pedometer view',
+    title: 'Pedometer View',
+  };
+  state = {
+    isPedometerAvailable: "checking",
+    pastStepCount: 0,
+    currentStepCount: 0
+  };
+
+  componentDidMount() {
+    this._subscribe();
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _subscribe = () => {
+    this._subscription = Pedometer.watchStepCount(result => {
+      this.setState({
+        currentStepCount: result.steps
+      });
+    });
+
+    Pedometer.isAvailableAsync().then(
+      result => {
+        this.setState({
+          isPedometerAvailable: String(result)
+        });
+      },
+      error => {
+        this.setState({
+          isPedometerAvailable: "Could not get isPedometerAvailable: " + error
+        });
+      }
+    );
+
+    const end = new Date();
+    const start = new Date();
+    start.setDate(end.getDate() - 1);
+    Pedometer.getStepCountAsync(start, end).then(
+      result => {
+        this.setState({ pastStepCount: result.steps });
+      },
+      error => {
+        this.setState({
+          pastStepCount: "Could not get stepCount: " + error
+        });
+      }
+    );
+  };
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
   };
 
   render() {
     return (
-      <Text>Pedometer screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Steps taken in the last 24 hours: {this.state.pastStepCount}
+        </Text>
+        <Text style={styles.text}>Walk! And watch this go up: {this.state.currentStepCount}</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#9DD6EB'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    padding: 10,
+  }
+});
+
+Expo.registerRootComponent(PedometerScreen);
