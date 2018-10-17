118ef6dbedb5d43d5e648f3d8a27a0e35bc33dcb
Tinus Flagstad <tinus@flagstad.as>
2018-10-17 11:45:52 +0200

    Add mock date to jest so that tests will pass no matter which day you run the tests #13

--- components/__tests__/TodoCalendar-test.js
+++ components/__tests__/TodoCalendar-test.js
@@ -7,6 +7,15 @@ import { createStore } from 'redux';
 const store = createStore(appState)
 
 it("TodoCalendar Matches snapshot.", () => {
+    // For å slippe at snapshottet failer fordi datoen har endres seg så faker vi datoen i dag.
+    const DATE_TO_USE = new Date('2016');
+    const _Date = Date;
+    global.Date = jest.fn(() => DATE_TO_USE);
+    global.Date.UTC = _Date.UTC;
+    global.Date.parse = _Date.parse;
+    global.Date.now = _Date.now;
+
+    
     const renderer = new ShallowRenderer();
     renderer.render(<TodoCalendar store={store} navigation={{ getParam: () => { }, setParams: () => { } }} isModalVisible={false} />);
 
